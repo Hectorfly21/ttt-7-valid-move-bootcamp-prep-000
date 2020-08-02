84d6035642713dd56f6abe3bb13093f85f8bf598
Hector <hectorfly21@gmail.com>
2020-08-02 01:07:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if !position_taken?(board, index) == false || (index).between?(0,8) == true
+  if !position_taken?(board, index)).to be_truthy && (index).between?(0,8)).to be_truthy
    return true
  else
    return false
